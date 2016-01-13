e4bc89a71c9be6cafe2a6a29e8fe6b276993e5b5
Somshubra Majumdar <titu1994@gmail.com>
2016-01-13 19:52:26 +0530

    XGBoost stack change to Logistic Regression

--- Homesite/StackedEnsemble/XGBStack.py
+++ Homesite/StackedEnsemble/XGBStack.py
@@ -2,6 +2,7 @@ import MLScripts.Ensemble.Stacking as stack
 import MLScripts.Helpers as helpers
 import Homesite.DataClean as dc
 import xgboost as xgb
+import sklearn.linear_model as linear
 
 trainFrame = dc.cleanData(dc.loadTrainData(), describe=False)
 testFrame = dc.cleanData(dc.loadTestData(), istest=True, describe=False)
@@ -14,15 +15,16 @@ xgb1 = xgb.XGBClassifier(max_depth=10, n_estimators=50, seed=0, learning_rate=0.
 xgb2 = xgb.XGBClassifier(max_depth=10, n_estimators=100, seed=0, learning_rate=0.1, subsample=0.9, colsample_bytree=0.8)
 xgb3 = xgb.XGBClassifier(max_depth=6, n_estimators=6000, seed=0, learning_rate=0.01, subsample=0.83, colsample_bytree=0.77)
 
-xgbBlend = xgb.XGBClassifier(max_depth=6, n_estimators=100, seed=0, learning_rate=0.1, subsample=0.83, colsample_bytree=0.77)
+#xgbBlend = xgb.XGBClassifier(max_depth=6, n_estimators=6000, seed=0, learning_rate=0.1, subsample=0.83, colsample_bytree=0.77)
+blend = linear.LogisticRegression(random_state=0, verbose=True, tol=1e-6)
 clfs = [xgb1, xgb2, xgb3]
 
-ensemble = stack.StackedClassifier(clfs, xgbBlend, verbose=True)
+ensemble = stack.StackedClassifier(clfs, blend, verbose=True)
 
 trainX = trainData[:, 1:]
 trainY = trainData[:, 0]
 
-ensemble.fit(trainX, trainY, useProbasInstead=True, xgb_eval_metric="auc", xgb_eval_set=[(trainData[:1000, 1:], trainData[:1000, 0])])
+ensemble.fit(trainX, trainY, xgb_eval_metric="auc", xgb_eval_set=[(trainData[:1000, 1:], trainData[:1000, 0])])
 
 testData = dc.convertPandasDataFrameToNumpyArray(testFrame)
 preds = ensemble.predict(testData[:, 1:])[:, 1]
