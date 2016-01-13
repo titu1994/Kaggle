d8b361431616dd4dece6940c13dbf80274993027
Somshubra Majumdar <titu1994@gmail.com>
2016-01-13 16:16:01 +0530

    Ensemble Model Stacked - Initial Failure

--- Homesite/StackedEnsemble/XGBStack.py
+++ Homesite/StackedEnsemble/XGBStack.py
@@ -11,10 +11,10 @@ trainData = dc.convertPandasDataFrameToNumpyArray(trainFrame)
 print("Data loaded")
 
 xgb1 = xgb.XGBClassifier(max_depth=10, n_estimators=50, seed=0, learning_rate=0.025, subsample=0.8, colsample_bytree=0.8)
-xgb2 = xgb.XGBClassifier(max_depth=10, n_estimators=100, seed=0, learning_rate=0.1, subsample=0.9, colsample_bytree=0.8)
-xgb3 = xgb.XGBClassifier(max_depth=6, n_estimators=6000, seed=0, learning_rate=0.01, subsample=0.83, colsample_bytree=0.77)
+xgb2 = xgb.XGBClassifier(max_depth=10, n_estimators=100, seed=1, learning_rate=0.1, subsample=0.9, colsample_bytree=0.8)
+xgb3 = xgb.XGBClassifier(max_depth=6, n_estimators=6000, seed=2, learning_rate=0.01, subsample=0.83, colsample_bytree=0.77)
 
-xgbBlend = xgb.XGBClassifier(max_depth=6, n_estimators=100, seed=0, learning_rate=0.1, subsample=0.83, colsample_bytree=0.77)
+xgbBlend = xgb.XGBClassifier(max_depth=6, n_estimators=1000, seed=0, learning_rate=0.01, subsample=0.83, colsample_bytree=0.77)
 clfs = [xgb1, xgb2, xgb3]
 
 ensemble = stack.StackedClassifier(clfs, xgbBlend, verbose=True)
@@ -25,7 +25,7 @@ trainY = trainData[:, 0]
 ensemble.fit(trainX, trainY, useProbasInstead=True, xgb_eval_metric="auc", xgb_eval_set=[(trainData[:1000, 1:], trainData[:1000, 0])])
 
 testData = dc.convertPandasDataFrameToNumpyArray(testFrame)
-preds = ensemble.predict(testData[:, 1:])[:, 1]
+preds = ensemble.predict(testData[:, 1:])
 
 helpers.writeOutputFile("stacked_ensemble.csv", headerColumns=["QuoteNumber", "QuoteConversion_Flag"],
                         submissionRowsList=[testData[:, 0], preds], dtypes=[int, float])
